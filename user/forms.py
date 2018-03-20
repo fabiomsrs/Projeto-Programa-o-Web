3e359272261309b0f711db37608cdab1d2cd46b1
fabiano <fabiomsrs@gmail.com>
2018-03-20 18:07:06 -0300

    arquivos .html e .py

---
+++ user/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Usuario
+
+class FormUsuario(forms.ModelForm):
+	password=forms.CharField(widget=forms.PasswordInput())
+	confirm_password=forms.CharField(widget=forms.PasswordInput())
+	
+	class Meta:
+		model = Usuario		
+		fields = ('email','first_name','password')
+
+	def clean(self):
+	    cleaned_data = super(FormUsuario, self).clean()
+	    password = cleaned_data.get("password")
+	    confirm_password = cleaned_data.get("confirm_password")
+
+	    if password != confirm_password:
+	        raise forms.ValidationError(
+	            "password and confirm_password does not match"
+		        )
